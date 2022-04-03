c885afcecbac1b253beba7a0447eb71c33363373
Slavomir Bircak <slavomir.bircak@sk.ibm.com>
2022-04-03 18:25:53 +0200

    Simple PHP

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
+	<h1>Simple PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
+	<h2>Simple PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
         <h2>GOEDEMIDDAG GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
 	</body> 
